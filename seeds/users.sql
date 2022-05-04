c344949bac7c705666f7f954ba0bedf379a4f724
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-05-04 23:38:59 +0000

    Create table data base and users,games tables

---
+++ seeds/users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, name, email, password)
+VALUES (1, 'Diego', 'diego@gmail.com', 'asdf');
+
+INSERT INTO users (id, name, email, password)
+VALUES (2, 'Donna', 'donna@gmail.com', 'asdf');
+
+INSERT INTO users (id, name, email, password)
+VALUES (3, 'DougDimmadome', 'dimsdaledimmadome@gmail.com', 'asdf');
