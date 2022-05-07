bb934b9a9c2d36ddf4a65d527d39e90c61da968e
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-07 17:58:40 -0400

    [update] add route for user ids and placeholder data to render on page for testing

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,21 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
+  router.get("/:id", (req, res) => {
+    /* Here, res.send is sending text only to page on browser
+    Here, req.params.id is capturing input after the "/" in the url, on the browser
+    res.send("X-user" + req.params.id);
+    */
+
+    // USe this as placeholder DATA for React before connecting the db
+    res.json({
+      id: req.params.id,
+      name: "TJ Blinn",
+    });
+  });
+
+  // router represents everything in this file express.Router()
+  //
   router.get("/", (req, res) => {
     res.send("users");
     // db.query(`SELECT * FROM users;`)
