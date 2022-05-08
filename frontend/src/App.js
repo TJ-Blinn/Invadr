25495063166619363faa70ad95196ca5d3bf2484
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-08 16:22:40 -0400

    create react app for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import Register from "./components/Register";
+import Homepage from "./components/Homepage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
