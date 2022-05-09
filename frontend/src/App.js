7da5ba9298ee614037ba330d6e2c67a885a8dc91
Hollie Hensman <hensmanh@gmail.com>
2022-05-09 18:29:10 -0400

    Create About page skeleton and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Register from "./components/Register";
 import Homepage from "./components/Homepage";
+import About from "./components/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/register" element={<Register />}></Route>
 
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </Router>
   );
