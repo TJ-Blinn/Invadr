763c753a575de50e219519705fd34c8d518f178d
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-09 15:48:26 -0400

    [update] changed classnames to confirm with material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import "./styles/register.css";
 import Register from "./components/Register";
 import Homepage from "./components/Homepage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -8,15 +7,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/register" element={<Register />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/register" element={<Register />}></Route>
 
-        <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-        </Routes>
-      </div>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
     </Router>
   );
 }
