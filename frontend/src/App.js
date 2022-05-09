d62d52f6d37e87cc39f658ad44d9607f82ec4065
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-08 23:17:12 -0400

    [update]import register.css to keep styling seperate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import "./styles/register.css";
 import Register from "./components/Register";
 import Homepage from "./components/Homepage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
