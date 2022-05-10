3043639ab8c94df6736a1f9b5782189f770e76ce
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-09 21:26:27 -0400

    [feature] add path for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Register from "./components/Register";
 import Homepage from "./components/Homepage";
+import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/register" element={<Register />}></Route>
-
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/" element={<Homepage />}></Route>
       </Routes>
     </Router>
