fcf1dfe27431f7e38a2be16eb7955389329dbdd4
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-10 12:58:48 -0400

    [update]Temp solution to display Nav Bar using fragments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,29 @@ import React from "react";
 import "./App.css";
 import Register from "./components/Register";
 import Homepage from "./components/Homepage";
+import Navigation from "./components/Navigation";
 import Login from "./components/Login";
-import About from "./components/About"; 
+import About from "./components/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* <Route path="/navigation" element={<Navigation />}></Route> */}
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/" element={<Homepage />}></Route>
+
+        {/* Temp solution to display Nav Bar using fragments */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Navigation />
+              <Homepage />
+            </>
+          }
+        ></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
     </Router>
