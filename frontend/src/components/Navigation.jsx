034e76f1f34adf70b914630179020f90c7d67ed3
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-10 13:57:37 -0400

    [update] made container for toolbar and menu and added styling

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "@mui/material";
-// import { makeStyles } from "@mui/styles";
+import { AppBar } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import { Container } from "@mui/material";
+import { Hidden } from "@mui/material";
 
 const navigationLinks = [
   { name: "Homepage", href: "/" },
@@ -9,30 +12,34 @@ const navigationLinks = [
   { name: "About", href: "/about" },
 ];
 
-// Adds space between the links on the nav pullout
-// const useStyles = makeStyles((theme) => ({
-//   link: {
-//     marginRight: 20,
-//   },
-// }));
-
 export default function Navigation() {
-  // const styles = useStyles();
-
+  // Adds space between the links on the nav pullout
+  const navStyles = {
+    marginRight: 20,
+  };
+  // AppBar and Toolbar MUI components to contain active elements, also adds padding and spacing
+  // Container is used here to put constraints on the toolbar when window is large
+  // Hidden MUI component is hidding the menu bar when screen is small
   return (
-    <div>
-      {navigationLinks.map((item) => (
-        <Link
-          // className={styles.link}
-          color="textPrimary"
-          variant="button"
-          underline="none"
-          href={item.href}
-          key={item.name}
-        >
-          {item.name}
-        </Link>
-      ))}
-    </div>
+    <AppBar position="sticky" color="default">
+      <Container maxWidth="md">
+        <Toolbar disableGutters>
+          <Hidden>
+            {navigationLinks.map((item) => (
+              <Link
+                sx={navStyles}
+                color="textPrimary"
+                variant="button"
+                underline="none"
+                href={item.href}
+                key={item.name}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </Hidden>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
