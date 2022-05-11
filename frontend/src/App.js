4fb8f3dffe9c568b9932d4d97e85ae2c3a922189
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-11 00:13:57 -0400

    [update] removed Navigation fragment here and moved it into each page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Register from "./components/Register";
 import Homepage from "./components/Homepage";
-import Navigation from "./components/Navigation";
 import Login from "./components/Login";
 import About from "./components/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -11,20 +10,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/navigation" element={<Navigation />}></Route> */}
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-
-        {/* Temp solution to display Nav Bar using fragments */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Navigation />
-              <Homepage />
-            </>
-          }
-        ></Route>
+        <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
     </Router>
