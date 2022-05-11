e9bf6e45f4e3597dbfb3fef2b3c39a83681ee8a5
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-11 00:15:46 -0400

    [update]add Navigation component to render on page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,29 +1,39 @@
 import React from "react";
-
-
+import Navigation from "./Navigation";
 
 export default function About() {
   return (
     <div className="container">
-
-     <nav class="nav">
-        <img class="hamburger" alt="filter button" src="https://previews.123rf.com/images/fokaspokas/fokaspokas1809/fokaspokas180900164/108564673-hamburger-menu-web-icon-white-icon-with-shadow-on-transparent-background.jpg" width="100" height="100"/>
-       <h1 class="page-name">INVADR</h1>
-        <img class="sign-up" alt="Sign Up" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ059ESnk10-TfyyVoKGpAz29cBnwTipRfW6w&usqp=CAU" width="100" height="100"/>
+      <Navigation />
+      <nav class="nav">
+        <img
+          class="hamburger"
+          alt="filter button"
+          src="https://previews.123rf.com/images/fokaspokas/fokaspokas1809/fokaspokas180900164/108564673-hamburger-menu-web-icon-white-icon-with-shadow-on-transparent-background.jpg"
+          width="100"
+          height="100"
+        />
+        <h1 class="page-name">INVADR</h1>
+        <img
+          class="sign-up"
+          alt="Sign Up"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ059ESnk10-TfyyVoKGpAz29cBnwTipRfW6w&usqp=CAU"
+          width="100"
+          height="100"
+        />
       </nav>
 
       {/* -------- NAV is the same accross all pages -------- */}
 
       <div>
         <h1 class="about">About</h1>
-        <p class="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam et eum labore! Illo reprehenderit deleniti sed mollitia adipisci? Earum quia nobis corrupti officiis atque nesciunt est alias commodi ex similique.</p>
+        <p class="about-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam et
+          eum labore! Illo reprehenderit deleniti sed mollitia adipisci? Earum
+          quia nobis corrupti officiis atque nesciunt est alias commodi ex
+          similique.
+        </p>
       </div>
-
-
-
-
-
-
     </div>
   );
 }
