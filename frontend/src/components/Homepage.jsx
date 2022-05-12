cc9ac9337fe4ec137a781e0ca04b8eab511aa2fa
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-05-11 22:39:02 -0400

    Fix select drop down to show genres from RAWG api

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "./Navigation";
+import axios from "axios";
 
 export default function Homepage() {
+  const [genre, setGenre ] = useState("");
+  const [results, setResults] = useState([]);
+
+  const startingURL = "https://api.rawg.io/api/genres?key=4d6e63aaf07b45ada62f971b8736e525";
+
+  const baseGenreURL ="https://api.rawg.io/api/games?key=d355ab68065146b29254681eac449af9&genres=";
+
+  // when a genre is selected, append the genre to baseGenreURL
+
+  useEffect(() => {
+    axios.get(startingURL).then(response => {
+      setResults([response.data.results])
+    });
+  }, []);
+
+  const resultsMapped = results[0].map(result => {
+    let value = result.name
+    return <option value={value}>{value}</option>
+  })
+
   return (
     <div>
       <nav class="nav">
@@ -26,9 +47,10 @@ export default function Homepage() {
       <div class="banner">
         <h1 class="banner-text">BEWARE THE INVASION</h1>
         <h2 class="banner-pick">Pick A Genre:</h2>
-        <select class="select-genre" name="genre"></select>
+        <select class="select-genre" name="genre">
+          {resultsMapped}
+        </select>
       </div>
-
       <h2 class="featured">Featured Games:</h2>
 
       <div class="game-container">
