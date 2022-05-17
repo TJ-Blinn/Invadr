a5631413ff4161d262b602707f0b6f9bffee30a8
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-05-16 23:12:10 -0400

    [update]removed games table

--- db/migrations/tables.sql
+++ db/migrations/tables.sql
@@ -7,15 +7,15 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE games (
-  id SERIAL PRIMARY KEY NOT NULL,
-  thumbnail_url VARCHAR(2048),
-  cover_url VARCHAR(2048),
-  name VARCHAR(255) NOT NULL,
-  genre VARCHAR(50),
-  description VARCHAR(2048) DEFAULT 'I am a description.',
-  rawg_id INTEGER
-);
+-- CREATE TABLE games (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   thumbnail_url VARCHAR(2048),
+--   cover_url VARCHAR(2048),
+--   name VARCHAR(255) NOT NULL,
+--   genre VARCHAR(50),
+--   description VARCHAR(2048) DEFAULT 'I am a description.',
+--   rawg_id INTEGER
+-- );
 
 CREATE TABLE likes (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -29,8 +29,8 @@ ON likes (user_id, game_id);
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users (id),
-  game_id INTEGER REFERENCES games (id),
+  user_id INTEGER ,
+  game_id INTEGER ,
   comment VARCHAR(2048),
   created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
